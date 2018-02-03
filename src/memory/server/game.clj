2eb9264fff2c51819fc8a522f2d009989f95a566
jp063 <jp063@hdm-stuttgart.de>
2018-02-03 12:49:54 +0100

    * refactor games map in games.clj * add users map to games.clj

--- src/memory/server/game.clj
+++ src/memory/server/game.clj
@@ -1,18 +1,13 @@
 (ns memory.server.game)
-(require 'digest)
 
-;; too long TODO: not random - same value always generates same id?
-(defn create-game-id [uid]
-  (digest/md5 uid))
 
 (defn create-new-game [player-one-uid]
   {
-   :id (create-game-id player-one-uid)
    :player-one {
-                :player player-one-uid
+                :uid player-one-uid
                 :resolved-pairs (list)}
    :player-two {
-                :player nil
+                :uid nil
                 :resolved-pairs (list)}
    :closed-cards (take 36 (iterate inc 0))
    :active-user (rand-int 1)})
--- src/memory/server/games.clj
+++ src/memory/server/games.clj
@@ -1,12 +1,18 @@
 (ns memory.server.games
   (:require
-    [memory.server.game :as game]))
+    [memory.server.game :as game]
+    [digest :as digest]))
+(def users (atom {}))
+(def games (atom {}))
 
-(def games (atom {:queue '()}))
+;; too long TODO: not random - same value always generates same id?
+(defn create-game-id [uid]
+  (digest/md5 uid))
 
 ;;does this append the single elements or append the whole map?
 (defn add-new-game [uid]
-  (def game (game/create-new-game uid))
-  (swap! games update-in [:queue] concat game)
-  (println @games)
-    game)
+  (let [game-id (create-game-id uid) game (game/create-new-game uid)]
+    (swap! games assoc-in [game-id] game)
+    (swap! users assoc-in [uid] game-id)
+    game
+   ))

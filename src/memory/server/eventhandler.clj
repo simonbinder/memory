26852b1d15243298eb49c7309efbf2a7c7327605
jp063 <jp063@hdm-stuttgart.de>
2018-02-09 16:30:54 +0100

    * eventhandler /handle-card-selected * eventhandler /determine-game-state * eventhandler /match? * eventhandler /filter-unresolved-cards * eventhandler /filter-turned-cards

--- src/memory/server/eventhandler.clj
+++ src/memory/server/eventhandler.clj
@@ -6,22 +6,40 @@
 (defn create-game [uid]
     (games/add-new-game uid))
 
-(declare handle-card-selected validate-player-action get-active-player-uid)
+(declare handle-card-selected validate-player-action get-active-player-uid determine-game-state)
 
 (defn handle-card-selected [uid game]
-  (validate-player-action uid game)
-  (deter)
+  (validate-player-action uid game))
 
-)
+(comment
+(defmulti forward-game-when determine-game-state)
+(defmethod forward-game-when :first-card-selected)
+(defmethod forward-game-when :cards-matching)
+(defmethod forward-game-when :cards-not-matching)
+(defmethod forward-game-when :game-finished))
 
-(declare determine-next-active-player determine-number-of-card-selected determine-game-state)
-(defmethod determine-game-state)
-(defmulti forward-game determine-game-state)
-(defmethod forward-game :first-card-selected)
-(defmethod forward-game :second-card-selected)
-(defmethod forward-game :first-card-selected)
+(defn determine-game-state [game]
+ (let [{:keys [deck]} game
+       unresolved (filter-unresolved-cards deck)
+       turned (filter-turned-cards unresolved)]
+   (when (= (count turned) (unresolved)) ;two cards are left, both are turned
+      :game-finished)
+   (when (= 1 (count turned))
+      :first-card-selected)
+   (when (= (count turned) 2)
+      ((if (match? turned)
+        :cards-matching
+        :cards-not-matching)))))
 
 
+(defn match? [[card-one card-two]]
+  (= (:url card-one) (:url card-two)))
+
+(defn filter-unresolved-cards [deck]
+  (filter #(= (:resolved %) 0)) deck)
+
+(defn filter-turned-cards [deck]
+  (filter #(true? (:turned %))) deck)
 
 (validate-player-action [sender-uid game]
   (if (not= sender-uid (get-active-player-uid game))

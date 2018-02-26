cd79bac7b52b51c954a06a404eaebfa857a9a73f
Sonja Nürenberg <sn043@hdm-stuttgart.de>
2018-02-26 19:42:01 +0100

    finished cleanup eventhandler and model

--- src/memory/client/eventhandler.cljs
+++ src/memory/client/eventhandler.cljs
@@ -2,19 +2,12 @@
   (:require
     [memory.client.model :as model]))
 
-(defn get-player-number[game]
-  (let [uid (:player-uid @model/app-state)
-        players (get game :players)
-        player-number (first (filter (comp #{uid} players) (keys players)))]
-        player-number))
-
 (defn set-uid [uid]
   (swap! model/app-state assoc :player-uid uid))
 
 (defn receive-game [server-game]
   (model/set-state 2)
-  ;(model/set-player-number)
-  (swap! model/app-state assoc :player-number (get-player-number server-game))
+  (model/set-player-number server-game)
   (model/set-game server-game))
 
 (defn finish-game [server-game]
--- src/memory/client/model.cljs
+++ src/memory/client/model.cljs
@@ -3,24 +3,24 @@
     [reagent.core :as reagent :refer [atom]]))
 
 (defonce game (atom
-          {:active-player 1
-           :players {}
-           :deck  [ {:id 0 :url "http://cdn.kickvick.com/wp-content/uploads/2014/11/cute-baby-animals-39.jpg" :turned false :resolved 0}
-                  {:id 1 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-31.jpg" :turned false :resolved 0}
-                  {:id 2 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg" :turned false :resolved 0}
-                  {:id 3 :url "https://static.geo.de/bilder/28/52/60898/galleryimage/01-baby-faultiere-kermie.jpg" :turned false :resolved 0}
-                  {:id 4 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-31.jpg" :turned false :resolved 0}
-                  {:id 5 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-13.jpg" :turned false :resolved 0}
-                  {:id 6 :url "https://winkgo.com/wp-content/uploads/2015/02/29-Tiny-Baby-Animals-so-Cute-They-Will-Take-Your-Cares-Away-01.jpg" :turned false :resolved 0}
-                  {:id 7 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-13.jpg" :turned false :resolved 0}
-                  {:id 8 :url "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC2b6U3sUWG3XWf0o-rCRN7KXhF9xvPAbBFht-gTsq8r1m1LeRug" :turned false :resolved 0}
-                  {:id 9 :url "https://winkgo.com/wp-content/uploads/2015/02/29-Tiny-Baby-Animals-so-Cute-They-Will-Take-Your-Cares-Away-01.jpg" :turned false :resolved 0}
-                  {:id 10 :url "http://media.einfachtierisch.de/thumbnail/600/0/media.einfachtierisch.de/images/2013/01/Junge-Katze-Erziehen.jpg" :turned false :resolved 0}
-                  {:id 11 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg" :turned false :resolved 0}
-                  {:id 12 :url "http://cdn.kickvick.com/wp-content/uploads/2014/11/cute-baby-animals-39.jpg" :turned false :resolved 0}
-                  {:id 13 :url "https://static.geo.de/bilder/28/52/60898/galleryimage/01-baby-faultiere-kermie.jpg" :turned false :resolved 0}
-                  {:id 15 :url "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC2b6U3sUWG3XWf0o-rCRN7KXhF9xvPAbBFht-gTsq8r1m1LeRug" :turned false :resolved 0}
-                  {:id 16 :url "http://media.einfachtierisch.de/thumbnail/600/0/media.einfachtierisch.de/images/2013/01/Junge-Katze-Erziehen.jpg" :turned false :resolved 0}]}))
+  {:active-player 1
+   :players {}
+   :deck  [ {:id 0 :url "http://cdn.kickvick.com/wp-content/uploads/2014/11/cute-baby-animals-39.jpg" :turned false :resolved 0}
+          {:id 1 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-31.jpg" :turned false :resolved 0}
+          {:id 2 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg" :turned false :resolved 0}
+          {:id 3 :url "https://static.geo.de/bilder/28/52/60898/galleryimage/01-baby-faultiere-kermie.jpg" :turned false :resolved 0}
+          {:id 4 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-31.jpg" :turned false :resolved 0}
+          {:id 5 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-13.jpg" :turned false :resolved 0}
+          {:id 6 :url "https://winkgo.com/wp-content/uploads/2015/02/29-Tiny-Baby-Animals-so-Cute-They-Will-Take-Your-Cares-Away-01.jpg" :turned false :resolved 0}
+          {:id 7 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-13.jpg" :turned false :resolved 0}
+          {:id 8 :url "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC2b6U3sUWG3XWf0o-rCRN7KXhF9xvPAbBFht-gTsq8r1m1LeRug" :turned false :resolved 0}
+          {:id 9 :url "https://winkgo.com/wp-content/uploads/2015/02/29-Tiny-Baby-Animals-so-Cute-They-Will-Take-Your-Cares-Away-01.jpg" :turned false :resolved 0}
+          {:id 10 :url "http://media.einfachtierisch.de/thumbnail/600/0/media.einfachtierisch.de/images/2013/01/Junge-Katze-Erziehen.jpg" :turned false :resolved 0}
+          {:id 11 :url "https://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-10.jpg" :turned false :resolved 0}
+          {:id 12 :url "http://cdn.kickvick.com/wp-content/uploads/2014/11/cute-baby-animals-39.jpg" :turned false :resolved 0}
+          {:id 13 :url "https://static.geo.de/bilder/28/52/60898/galleryimage/01-baby-faultiere-kermie.jpg" :turned false :resolved 0}
+          {:id 15 :url "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC2b6U3sUWG3XWf0o-rCRN7KXhF9xvPAbBFht-gTsq8r1m1LeRug" :turned false :resolved 0}
+          {:id 16 :url "http://media.einfachtierisch.de/thumbnail/600/0/media.einfachtierisch.de/images/2013/01/Junge-Katze-Erziehen.jpg" :turned false :resolved 0}]}))
 
 ;; state
 ;; 0 -> not started (options start new game or join)
@@ -32,27 +32,6 @@
 (defonce game-count (atom {:own-score 0 :opponent-score 0}))
 
 (defonce error (atom ""))
-
-(defn set-game-id [game-id]
-  (swap! app-state assoc :game-id (str game-id)))
-
-(defn set-state [state]
-  (swap! app-state assoc :state state))
-
-(defn set-game [new-game]
-  (print new-game)
-  (swap! game assoc :active-player (get new-game :active-player))
-  (swap! game assoc :players (get new-game :players))
-  (swap! game assoc :deck (get new-game :deck))
-  (refresh-game-count))
-
-(defn set-player-number []
-  (if (not= 0 (:player-number @app-state))
-    (do
-      (let [uid (:player-uid @app-state)
-          players (:players @game)
-          player-number (first (filter (comp #{uid} players) (keys players)))]
-      (swap! app-state assoc :player-number player-number)))))
 
 ; -------------------------------------------------------------------------------------------------
 ; FUNCTIONS TO CALCULATE SCORES AND REFRESH THEM
@@ -68,6 +47,31 @@
   opponent-score (count-unresolved-cards deck opponent-number)]
   (swap! game-count assoc :own-score own-score)
   (swap! game-count assoc :opponent-score opponent-score)))
+  
+; -------------------------------------------------------------------------------------------------
+; FUNCTIONS TO SET NEW VALUES
+
+(defn set-game-id [game-id]
+  (swap! app-state assoc :game-id (str game-id)))
+
+(defn set-state [state]
+  (swap! app-state assoc :state state))
+
+(defn set-game [new-game]
+  (print new-game)
+  (swap! game assoc :active-player (get new-game :active-player))
+  (swap! game assoc :players (get new-game :players))
+  (swap! game assoc :deck (get new-game :deck))
+  (refresh-game-count))
+
+(defn set-player-number [game]
+  (if (= 0 (:player-number @app-state))
+    (do
+      (print "hier bin ich")
+      (let [uid (:player-uid @app-state)
+          players (:players game)
+          player-number (first (filter (comp #{uid} players) (keys players)))]
+      (swap! app-state assoc :player-number player-number)))))
 
 ; -------------------------------------------------------------------------------------------------
 ; FUNCTIONS TO TURN CARD AND CHECK IF PLAYER IS ALLOWED TO

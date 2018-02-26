78f7371e0f342db182e66a538b8254bfe2da3c9d
sb220 <sb220@hdm-stuttgart.de>
2018-02-26 17:15:11 +0100

    added finish screen

--- src/memory/client/eventhandler.cljs
+++ src/memory/client/eventhandler.cljs
@@ -17,7 +17,7 @@
                active-player (get server-game :active-player)]
          (swap! model/app-state assoc :state 2)
          (if (= 2 (@model/app-state :turned-cards))
-            (swap! model/app-state assoc :turned-cards 0))
+            (swap! model/app-state assoc :turned-cards 0)) 
          (swap! model/app-state assoc :player-number (get-player-number server-game))
          (println "Server-Game: " server-game)
          (swap! model/game assoc :deck deck)
@@ -29,6 +29,18 @@
          (println "Client-Game: "@model/game)
          (println "Game-count "@model/game-count)
          (println @model/app-state)))
+
+(defn finish-game [server-game]
+  (let [deck (get server-game :deck)
+        players (get server-game :players)
+         active-player (get server-game :active-player)]
+   (swap! model/app-state assoc :state 4)
+   (swap! model/game assoc :deck deck)
+   (swap! model/game assoc :active-player active-player)
+   (swap! model/game assoc :players players)
+   (let [[own-score opponent-score] (model/calc-game-count)]
+   (swap! model/game-count assoc :own-score own-score)
+   (swap! model/game-count assoc :opponent-score opponent-score))))
 
 (defn set-to-wait []
         (swap! model/app-state assoc :state 3))
